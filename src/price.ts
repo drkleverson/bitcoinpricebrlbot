81599c5b4baba2a780a2201d4e50f62d08f20b91
Kleverson Bona <me@kleverson.dev>
2023-08-11 14:44:51 -0300

    refactor: prettier formatter

--- src/price.ts
+++ src/price.ts
@@ -1,15 +1,19 @@
-import 'dotenv/config';
+import "dotenv/config";
 import { client } from "./connection/twitter";
 import * as tools from "./tools";
-import axios from 'axios'
-import { env } from './env';
+import axios from "axios";
+import { env } from "./env";
 
 export default async () => {
-  const result: any = await axios.get('/api/v3/simple/price', {
+  const result: any = await axios.get("/api/v3/simple/price", {
     baseURL: "https://api.coingecko.com",
-    params: { ids: 'bitcoin', vs_currencies: 'brl,usd,eur', include_24hr_change: true },
-    responseType: 'json'
-  })
+    params: {
+      ids: "bitcoin",
+      vs_currencies: "brl,usd,eur",
+      include_24hr_change: true,
+    },
+    responseType: "json",
+  });
 
   const change = result.data.bitcoin.usd_24h_change;
   const negative = change > 0 ? "+" : "";
@@ -25,4 +29,4 @@ variação 24 horas: ${negative + tools.moneyFormat(change)}%
   if (!env.isDev) {
     client.v2.tweet({ text: tweet });
   }
-}
+};
--- src/statistics.ts
+++ src/statistics.ts
@@ -1,38 +1,65 @@
-import 'dotenv/config';
+import "dotenv/config";
 import { client } from "./connection/twitter";
-import axios from 'axios';
-import { env } from './env';
+import axios from "axios";
+import { env } from "./env";
 
 export default async () => {
-    const result: any = await axios.get('/api/v3/coins/bitcoin', {
-        baseURL: "https://api.coingecko.com",
-        params: { sparkline: false, community_data: false, developer_data: false, localization: false, tickers: false },
-        responseType: 'json'
-    })
+  const result: any = await axios.get("/api/v3/coins/bitcoin", {
+    baseURL: "https://api.coingecko.com",
+    params: {
+      sparkline: false,
+      community_data: false,
+      developer_data: false,
+      localization: false,
+      tickers: false,
+    },
+    responseType: "json",
+  });
 
-    const data = result.data.market_data
+  const data = result.data.market_data;
 
-    let max24hrs = data.high_24h.usd.toLocaleString('pt-BR', { style: 'currency', currency: 'USD' });
-    let min24hrs = data.low_24h.usd.toLocaleString('pt-BR', { style: 'currency', currency: 'USD' });
-    let priceChange7d = data.price_change_percentage_7d.toFixed(2)
-    let priceChange7dString = `7 dias: ${priceChange7d > 0 ? '+' : ''}${priceChange7d}%`
-    let priceChange30d = data.price_change_percentage_30d.toFixed(2)
-    let priceChange30dString = `30 dias: ${priceChange30d > 0 ? '+' : ''}${priceChange30d}%`
-    let priceChange60d = data.price_change_percentage_60d.toFixed(2)
-    let priceChange60dString = `60 dias: ${priceChange60d > 0 ? '+' : ''}${priceChange60d}%`
-    let priceChange1y = data.price_change_percentage_1y.toFixed(2)
-    let priceChange1yString = `1 ano: ${priceChange1y > 0 ? '+' : ''}${priceChange1y}%`
-    let marketcap = data.market_cap.usd.toLocaleString('pt-BR', { style: 'currency', currency: 'USD' });
-    let marketcapString = `total: ${marketcap}`;
-    let marketcapChange = data.market_cap_change_percentage_24h.toFixed(2)
-    let marketcapChangeString = `24hrs: ${marketcapChange > 0 ? '+' : ''}${marketcapChange}%`
-    let supplyString = `${data.circulating_supply.toLocaleString('pt-BR')} de ${data.max_supply.toLocaleString('pt-BR')}`
+  let max24hrs = data.high_24h.usd.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "USD",
+  });
+  let min24hrs = data.low_24h.usd.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "USD",
+  });
+  let priceChange7d = data.price_change_percentage_7d.toFixed(2);
+  let priceChange7dString = `7 dias: ${
+    priceChange7d > 0 ? "+" : ""
+  }${priceChange7d}%`;
+  let priceChange30d = data.price_change_percentage_30d.toFixed(2);
+  let priceChange30dString = `30 dias: ${
+    priceChange30d > 0 ? "+" : ""
+  }${priceChange30d}%`;
+  let priceChange60d = data.price_change_percentage_60d.toFixed(2);
+  let priceChange60dString = `60 dias: ${
+    priceChange60d > 0 ? "+" : ""
+  }${priceChange60d}%`;
+  let priceChange1y = data.price_change_percentage_1y.toFixed(2);
+  let priceChange1yString = `1 ano: ${
+    priceChange1y > 0 ? "+" : ""
+  }${priceChange1y}%`;
+  let marketcap = data.market_cap.usd.toLocaleString("pt-BR", {
+    style: "currency",
+    currency: "USD",
+  });
+  let marketcapString = `total: ${marketcap}`;
+  let marketcapChange = data.market_cap_change_percentage_24h.toFixed(2);
+  let marketcapChangeString = `24hrs: ${
+    marketcapChange > 0 ? "+" : ""
+  }${marketcapChange}%`;
+  let supplyString = `${data.circulating_supply.toLocaleString(
+    "pt-BR"
+  )} de ${data.max_supply.toLocaleString("pt-BR")}`;
 
-    let tweet = `máxima 24hrs: ${max24hrs}\nmínima 24hrs: ${min24hrs}\n\nfornecimento:\n${supplyString}\n\ncap. de mercado:\n${marketcapChangeString}\n${marketcapString}\n\npreço:\n${priceChange7dString}\n${priceChange30dString}\n${priceChange60dString}\n${priceChange1yString}`;
+  let tweet = `máxima 24hrs: ${max24hrs}\nmínima 24hrs: ${min24hrs}\n\nfornecimento:\n${supplyString}\n\ncap. de mercado:\n${marketcapChangeString}\n${marketcapString}\n\npreço:\n${priceChange7dString}\n${priceChange30dString}\n${priceChange60dString}\n${priceChange1yString}`;
 
-    console.log(tweet);
+  console.log(tweet);
 
-    if (!env.isDev) {
+  if (!env.isDev) {
     client.v2.tweet({ text: tweet });
-    }
+  }
 };
