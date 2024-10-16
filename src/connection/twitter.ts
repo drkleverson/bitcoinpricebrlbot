a86223fcc1404d516d24ca05b38a1e2ff1a63fac
Kleverson Bona <me@kleverson.dev>
2024-10-16 09:59:45 -0300

    refactor: add x client (rip twitter)

--- src/connection/twitter.ts
+++
@@ -1,9 +0,0 @@
-import { TwitterApi } from "twitter-api-v2";
-import { env } from "../env";
-
-export const client = new TwitterApi({
-  appKey: env.API_KEY,
-  appSecret: env.API_KEY_SECRET,
-  accessToken: env.ACCESS_TOKEN,
-  accessSecret: env.ACCESS_TOKEN_SECRET,
-});
